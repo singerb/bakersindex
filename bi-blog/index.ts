23c728e986fa1afb24ee8f2204c5ab99cf7c1b5e
Benedict Singer <singerb@mac.com>
2026-01-19 20:48:56 -0500

    Move to docs/, add Pulumi

---
+++ bi-blog/index.ts
@@ -0,0 +1,44 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as github from "@pulumi/github";
+import * as aws from "@pulumi/aws";
+
+const config = new pulumi.Config();
+
+const baseDomain = config.require("domain");
+const targetDomain = `blog.${baseDomain}`;
+
+// Retrieve information about the currently authenticated user.
+const currentUser = github.getUser({
+    username: "",
+});
+
+const example = new github.Repository("bakersindex", {
+	name: "bakersindex",
+	hasDiscussions: true,
+	hasDownloads: true,
+	hasIssues: true,
+	hasProjects: true,
+	deleteBranchOnMerge: true,
+	pages: {
+		buildType: "legacy",
+		source: {
+			branch: "main",
+			path: "/docs",
+		},
+		cname: "blog.bakersindex.com",
+	}
+}, {
+	import: "bakersindex"
+});
+
+// assumes a Route53 Hosted Zone already exists for the domain
+const hostedZone = aws.route53.getZone({ name: baseDomain });
+
+const record = new aws.route53.Record(targetDomain, {
+	name: targetDomain,
+	zoneId: hostedZone.then(z => z.zoneId),
+	type: "CNAME",
+	ttl: 300,
+	records: currentUser.then(u => [`${u.name}.github.io`]),
+});
+
