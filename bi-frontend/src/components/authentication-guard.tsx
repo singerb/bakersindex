0030e5b34ac38fd7dcf7f2ec7ef6df16cef06313
Benedict Singer <singerb@mac.com>
2026-01-21 11:05:27 -0500

    Fix the loading state; otherwise working fine

--- bi-frontend/src/components/authentication-guard.tsx
+++ bi-frontend/src/components/authentication-guard.tsx
@@ -1,6 +1,6 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 import React, { type ComponentType } from "react";
-// import { PageLoader } from "./page-loader";
+import { Spinner } from "@/components/ui/spinner";
 
 interface AuthenticationGuardProps {
   component: ComponentType;
@@ -10,11 +10,12 @@ export const AuthenticationGuard: React.FC<AuthenticationGuardProps> = ({
   component,
 }) => {
   const Component = withAuthenticationRequired(component, {
-    // onRedirecting: () => (
-    //   <div className="page-layout">
-    //     <PageLoader />
-    //   </div>
-    // ),
+    onRedirecting: () => (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <Spinner className="size-24" />
+        <p className="font-bold">Loading..</p>
+      </div>
+    ),
   });
 
   return <Component />;
